c0f1dc0537f5b448e7da8f3b37805a98c85c2423
Brandon King <brandonscottking@gmail.com>
2021-03-19 19:19:25 -0400

    add click events on project cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
 
 
 //mobile menu event listener
-$(document).ready(function() {
+$(document).ready(function () {
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function(event) {
-      event.preventDefault();
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+  $(".navbar-burger").click(function (event) {
+    event.preventDefault();
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 
   });
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
 //add section headers
 const sections = document.querySelectorAll('section')
-for (let i =0; i < sections.length; i++) {
+for (let i = 0; i < sections.length; i++) {
   let header = document.createElement('h2')
   header.classList.add('section-header');
   header.textContent = (sections[i].id).toUpperCase();
@@ -25,11 +25,11 @@ for (let i =0; i < sections.length; i++) {
 
 //style contact icons
 const iconSpans = $('.icon')
-for (let i=0; i < iconSpans.length; i++){
+for (let i = 0; i < iconSpans.length; i++) {
   $(iconSpans[i]).addClass('is-large')
 }
 const iconImage = $('i');
-for (let j=0; j < iconImage.length; j++){
+for (let j = 0; j < iconImage.length; j++) {
   $(iconImage[j]).addClass('fa-3x')
 }
 
@@ -37,4 +37,12 @@ for (let j=0; j < iconImage.length; j++){
 let cards = $('.card');
 for (let x = 1; x < cards.length; x++) {
   $(cards[x]).css('width', '40%')
+}
+
+//cards click event
+for (let z = 0; z < cards.length; z++) {
+  $(cards[z]).on('click', (event) => {
+    event.preventDefault();
+    window.open(`https://thebsking.github.io/${$(cards[z]).attr('id')}`)
+  })
 }
